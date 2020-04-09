301669ccd83b47fddb7e559ed4dcff1c65d07946
tarosh3 <42916232+tarosh3@users.noreply.github.com>
2020-04-10 01:09:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+var drumnum=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<drumnum;i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click",function ()
+  { makesound(this.innerHTML);
+    ani(this.innerHTML);
+  });
+}
+
+document.addEventListener("keypress",function(event){
+  makesound(event.key);
+  ani(event.key);
+})
+
+function makesound(key)
+{
+  switch(key)
+  {
+    case 'w': var audio = new Audio("sounds/tom-1.mp3");
+              audio.play();
+              break;
+    case 'a': var audio = new Audio("sounds/tom-2.mp3");
+              audio.play();
+              break;
+    case 's': var audio = new Audio("sounds/tom-3.mp3");
+              audio.play();
+              break;
+    case 'd': var audio = new Audio("sounds/tom-4.mp3");
+              audio.play();
+              break;
+    case 'j': var audio = new Audio("sounds/snare.mp3");
+              audio.play();
+              break;
+    case 'k': var audio = new Audio("sounds/crash.mp3");
+              audio.play();
+              break;
+    case 'l': var audio = new Audio("sounds/kick-bass.mp3");
+              audio.play();
+              break;
+    default : console.log();
+
+  }
+}
+
+function ani(currentKey)
+{
+  var curBut=document.querySelector("."+currentKey);
+  curBut.classList.add("pressed");
+
+  setTimeout(function()
+  {
+    curBut.classList.remove("pressed");
+  },100);
+}
